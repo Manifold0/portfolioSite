308387754a74dbac7f5781a3f0dec0a3a3f10864
Nathan Cox <querky1231@aim.com>
2013-10-23 18:43:34 -0400

    Intial Commit

---
+++ portfolioSite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PortfolioSite::Application.config.secret_key_base = '70a69bd79718b7501f44231f557842ff3333bbcd2b67d2bcfba9e9fef79edd6da5f48eb97ccfd5b9e6cf64580bdd4e165ca1ec29ebf5ca12274368e20e69445a'
